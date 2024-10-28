54dd80b7e5173265e12a6e021d5f249e8b07438a
Mark Barnes <barnemar002@warren.k12.in.us>
2024-10-28 12:40:24 -0400

    initial

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+
+        Library A = new Library();
+        A.name = "A";
+        A.addBooks(100);
+        Library B = new Library();
+        B.name = "B";
+        B.addBooks(150);
+
+        System.out.println("Total amount of books in libraries: " + Library.getTotalBooks());
+    }
+
+
+}
